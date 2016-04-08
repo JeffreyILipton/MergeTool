a4eb8ba141ffe7dc1e021df712409bbaca46be97
Jeffrey Lipton <jeffreyilipton@gmail.com>
2016-04-07 21:20:06 -0400

    filled with QDatastreem

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <o3dpfile.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    O3DPFile f;
+    f.read("fingertip-solid.o3dp");
 
     return a.exec();
 }
--- o3dpfile.cpp
+++ o3dpfile.cpp
@@ -1,15 +1,20 @@
 #include "o3dpfile.h"
 #include <QFile>
+#include <QDataStream>
+#include <QString>
 #include <QDebug>
 
 O3DPFile::O3DPFile(QObject *parent) : QObject(parent)
 {
-
+    voxel_dimension = QVector<float>(3,0.0);
+    gridSize = QVector<qint32>(3,0.0);
+    bboxSize = QVector<double>(6,0.0);
+    num_mat = 0;
 }
 
 
 void O3DPFile::read(QString file){
-    QString cookie = '#OpenFab3DP V1.0 Binary';
+    QString cookie = "#OpenFab3DP V1.0 Binary";
     int headercount = cookie.length();
     int n_grid_size_units = 3;
     int n_bbox_units = 6;
@@ -19,16 +24,53 @@ void O3DPFile::read(QString file){
     headercount += n_bbox_units*8;
     headercount += n_mat_len;
 
-    QFile file("file.dat");
-    file.open(QIODevice::ReadOnly);
-    QDataStream in(&file);
+    QFile f(file);
+    f.open(QIODevice::ReadOnly);
+    QDataStream in(&f);
     QString readcookie;
     in>>readcookie;
-    qDebug<<"read cookie: "<<readcookie;
-    for(int i=0;i<3;i++){
+    qDebug()<<"read cookie: "<<readcookie;
+
+    // load int32 grid_val
+    for(int i=0;i<n_grid_size_units;i++){
         qint32 grid_val;
         in>>grid_val;
         this->gridSize[i]=grid_val;
+    }
+    // load float64
+    for(int i=0;i<n_bbox_units;i++){
+        double bbox_val;
+        in>>bbox_val;
+        this->bboxSize[i]=bbox_val;
+    }
+    // load num mat
+    in>>num_mat;
+
+    qDebug()<<"Gridsize: "<<this->gridSize;
+    qDebug()<<"bbox:" <<this->bboxSize;
+
+
+    for(int i=0;i<3;i++){
+        voxel_dimension[0+i] = (bboxSize[3+i]-bboxSize[0+i])/gridSize[i];
+    }
+    for(int z=0; z<this->gridSize[2]; z++){
+
+        QVector< QVector<quint8>>cols(gridSize[1]);
+        for(int y=0; y<this->gridSize[1]; y++){
+
+            QVector<quint8>row(gridSize[0],1);
+            cols[y] = row;
+        }
+        grid.append(cols);
+    }
+
+    for(int z=0; z<this->gridSize[2]; z++){
+        qDebug()<<"z:"<<z;
+        for(int y=0; y<this->gridSize[1]; y++){
+            for(int x=0; x<this->gridSize[0]; x++){
+                in>>grid[z][y][x];
+            }
+        }
     }
 
 
@@ -54,9 +96,9 @@ fwrite(file, numMaterials, 'uint32'); %leave unchanged'''
 
 
 QFile f(file);
-if( file.open( QFile::WriteOnly ) ){
-    QDataStream out(&file);
-    out <<'#OpenFab3DP V1.0 Binary';
+if( f.open( QFile::WriteOnly ) ){
+    QDataStream out(&f);
+    out <<"#OpenFab3DP V1.0 Binary";
     for (int i=0; i<this->gridSize.length();i++){
         out<<this->gridSize.at(i);
     }
@@ -69,7 +111,7 @@ if( file.open( QFile::WriteOnly ) ){
     for(int z=0; z<this->gridSize.at(2); z++){
         for(int y=0; z<this->gridSize.at(1); y++){
             for(int x=0; x<this->gridSize.at(0); y++){
-                out<<this->grid[x][y][z];
+                out<<this->grid[z][y][x];
             }
         }
     }
